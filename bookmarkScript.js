117f220871ea7a4eb09389919a94cac5ee46c83e
e331-1 <85487233+e331-1@users.noreply.github.com>
2022-07-25 15:18:36 +0900

    Update bookmarkScript.js

--- bookmarkScript.js
+++ bookmarkScript.js
@@ -1,5 +1,6 @@
 for(const element of document.getElementsByTagName('a')){
     if(element.href.indexOf('https://www.youtube.com/watch?v=')!=-1){
-        element.href=element.href.replace('www.youtube.com/watch?v=','e331.f5.si/y/play.php?v=')
+        element.href=element.href.replace('www.youtube.com/watch?v=','192.168.0.40/y/play.php?v=');
+        element.style.color='green'
     }
 }
